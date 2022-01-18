7e0ed636e7c45c0b88d804e848ac0a6b50033471
Mike Moore <michaelhmoore1@gmail.com>
2022-01-17 17:37:12 -0700

    abstracted image names and days; added dark mode

--- SwiftUIWeather/SwiftUIWeather/ContentView.swift
+++ SwiftUIWeather/SwiftUIWeather/ContentView.swift
@@ -7,28 +7,65 @@
 
 import SwiftUI
 
+enum Day: String {
+    case monday = "MON"
+    case tuesday = "TUE"
+    case wednesday = "WED"
+    case thursday = "THU"
+    case friday = "FRI"
+    case saturday = "SAT"
+    case sunday = "SUN"
+}
+
+enum WeatherImage: String {
+    case moonAndStars = "moon.stars.fill"
+    case moon = "moon.fill"
+    case cloud = "cloud.fill"
+    case snow = "snowflake"
+    case sun = "sun.max.fill"
+    case sunAndCloud = "cloud.sun.fill"
+    case rainAndLightning = "cloud.bolt.rain.fill"
+    case rainAndCloud = "cloud.rain.fill"
+    case rainAndMoon = "cloud.moon.rain.fill"
+    case wind = "wind"
+}
+
 struct ContentView: View {
+    @State private var isNight = false
+    
     var body: some View {
         ZStack {
-            BackgroundView(topColor: Color("darkBlue"), bottomColor: Color("lightBlue"))
+            BackgroundView(isNight: $isNight)
             VStack {
                 CityTextView(cityName: "Denver, CO")
-                CurrentWeatherView(imageName: "cloud.sun.fill", temperature: 76)
+                CurrentWeatherView(imageName: isNight ? .moon : .sunAndCloud, temperature: 76)
                 
                 HStack(spacing: 20) {
-                    WeatherDayView(day: "TUE", imageName: "cloud.sun.fill", temperature: 74)
-                    WeatherDayView(day: "WED", imageName: "wind", temperature: 68)
-                    WeatherDayView(day: "THU", imageName: "snowflake", temperature: 8)
-                    WeatherDayView(day: "FRI", imageName: "cloud.bolt.rain.fill", temperature: 65)
-                    WeatherDayView(day: "SAT", imageName: "cloud.rain.fill", temperature: 67)
-                    WeatherDayView(day: "SUN", imageName: "sun.max.fill", temperature: 102)
+                    WeatherDayView(day: .tuesday,
+                                   imageName: isNight ? .moonAndStars : .sunAndCloud,
+                                   temperature: 74)
+                    WeatherDayView(day: .wednesday,
+                                   imageName: isNight ? .moonAndStars : .wind,
+                                   temperature: 68)
+                    WeatherDayView(day: .thursday,
+                                   imageName: isNight ? .cloud : .snow,
+                                   temperature: -8)
+                    WeatherDayView(day: .friday,
+                                   imageName: isNight ? .moonAndStars : .rainAndLightning,
+                                   temperature: 65)
+                    WeatherDayView(day: .saturday,
+                                   imageName: isNight ? .rainAndMoon : .rainAndCloud,
+                                   temperature: 67)
+                    WeatherDayView(day: .sunday,
+                                   imageName: isNight ? .moon : .sun,
+                                   temperature: 102)
                 }
                 Spacer()
                 
                 Button {
-                    print("tapped")
+                    isNight.toggle()
                 } label: {
-                    WeatherButton(title: "Change Day Time", textColor: .blue, backgroundColor: .white)
+                    WeatherButton(isNight: $isNight, textColor: .blue, backgroundColor: .white)
                 }
                 Spacer()
                 
@@ -44,16 +81,16 @@ struct ContentView_Previews: PreviewProvider {
 }
 
 struct WeatherDayView: View {
-    var day: String
-    var imageName: String
+    var day: Day
+    var imageName: WeatherImage
     var temperature: Int
     
     var body: some View {
         VStack {
-            Text(day)
+            Text(day.rawValue)
                 .font(.system(size: 16, weight: .medium))
                 .foregroundColor(.white)
-            Image(systemName: imageName)
+            Image(systemName: imageName.rawValue)
                 .renderingMode(.original)
                 .resizable()
                 .aspectRatio(contentMode: .fit)
@@ -66,11 +103,10 @@ struct WeatherDayView: View {
 }
 
 struct BackgroundView: View {
-    var topColor: Color
-    var bottomColor: Color
+    @Binding var isNight: Bool
     
     var body: some View {
-        LinearGradient(gradient: Gradient(colors: [topColor, bottomColor]),
+        LinearGradient(gradient: Gradient(colors: [isNight ? .black : Color("darkBlue"), isNight ? Color("darkerBlue") : Color("lightBlue")]),
                        startPoint: .topLeading,
                        endPoint: .bottomTrailing)
             .edgesIgnoringSafeArea(.all)
@@ -89,12 +125,12 @@ struct CityTextView: View {
 }
 
 struct CurrentWeatherView: View {
-    var imageName: String
+    var imageName: WeatherImage
     var temperature: Int
     
     var body: some View {
         VStack(spacing: 8) {
-            Image(systemName: imageName)
+            Image(systemName: imageName.rawValue)
                 .renderingMode(.original)
                 .resizable()
                 .aspectRatio(contentMode: .fit)
@@ -109,12 +145,12 @@ struct CurrentWeatherView: View {
 }
 
 struct WeatherButton: View {
-    var title: String
+    @Binding var isNight: Bool
     var textColor: Color
     var backgroundColor: Color
     
     var body: some View {
-        Text(title)
+        Text(isNight ? "Day Time" : "Night Time")
             .frame(width: 280, height: 50)
             .background(backgroundColor)
             .foregroundColor(textColor)
